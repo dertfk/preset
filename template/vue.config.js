e41eb5c0f5342e98f161742a277e53318ca17c1f
胡皓 <hao.hu@changhong.com>
2019-12-06 16:38:40 +0800

    add template

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,7 +1,7 @@
 // 具体字段参见https://cli.vuejs.org/zh/config/#vue-config-js
 module.exports = {
   // 部署应用包时的基本 URL
-  publicPath: "/",
+  publicPath: "./",
   // npm run build时 生成文件的目录
   outputDir: "dist",
   // 如果不需要生产时的源映射，将此设置为false可以加速生产构建
