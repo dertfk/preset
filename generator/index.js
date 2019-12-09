eea2d5003b63ca79f8e88eea616f8cfc79bc8dff
胡皓 <hao.hu@changhong.com>
2019-12-09 14:13:24 +0800

    update

--- generator/index.js
+++ generator/index.js
@@ -1,21 +1,15 @@
 module.exports = (api, options, rootOptions) => {
+  // 修改 `package.json` 里的字段
   api.extendPackage({
-    dependencies: {
-      'axios': '^0.19.0'
+    scripts: {
+      test: 'jest'
     }
   })
-  // 修改 `package.json` 里的字段
-  // api.extendPackage({
-    // scripts: {
-      // test: 'jest'
-    // }
-  // })
 
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('./template')
-  
-  api.render(files => {
-    Object.keys(files)
-      .forEach(path => console.log(files[path]));
+
+  api.render({
+    './rendertest.txt'     : './rendertest.txt'
   });
 }
