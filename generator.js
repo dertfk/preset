74e498382d1c27fb60a61fc7c8b844c9e192ead6
dertfk <haohaoblue@163.com>
2019-12-09 10:40:55 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -7,5 +7,10 @@ module.exports = (api, options, rootOptions) => {
   })
 
   // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('./template')
+  // api.render('./template')
+  api.render(files => {
+    files.foreach(file => {
+      console.log(file.path)
+    }
+  })
 }
