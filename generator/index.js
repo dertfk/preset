080b1492a8581fb556e6822948e650b1fa7a3427
胡皓 <hao.hu@changhong.com>
2019-12-10 09:05:11 +0800

    update template

--- generator/index.js
+++ generator/index.js
@@ -1,20 +1,17 @@
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
-  api.extendPackage({
-    scripts: {
-      test: 'jest'
-    }
-  })
+  // api.extendPackage({
+    // scripts: {
+      // test: 'jest'
+    // }
+  // })
+  // 复制并用 ejs 渲染 `./template` 内所有的文件
+  // api.render('./template')
 
-  // api.render(files => {
-    // Object.keys(files)
-      // .filter(path => path.startsWith('src/') || path.startsWith('public/'))
-      // .forEach(path => delete files[path]);
+  // api.render({
+    // './prettify.js'     : './prettify.js'
   // });
   
-  // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('./template')
-
   api.render({
     './rendertest.txt'     : './rendertest.txt'
   });
--- generator/template/src/main.js
+++ generator/template/src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+let a = 0;
 import store from "./store";
+
 
 Vue.config.productionTip = false;
 
